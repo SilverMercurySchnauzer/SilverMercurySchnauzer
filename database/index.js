7caf17cb4b930771d50d647b496bab65eaca9071
Michael Thomasson <mjthomasson@gmail.com>
2018-09-08 15:00:49 -0500

    working on deployment

--- database/index.js
+++ database/index.js
@@ -1,14 +1,23 @@
-const mysql = require('pg');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'FILL_ME_IN',
-  database : 'test'
+// const connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'FILL_ME_IN',
+//   database : 'test'
+// });
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true == process.env.SSL
 });
 
+
+
 const selectAll = callback => {
-  connection.query('SELECT * FROM items', (err, results, fields) => {
+  pool.query('SELECT * FROM items', (err, results, fields) => {
     if(err) {
       callback(err, null);
     } else {
