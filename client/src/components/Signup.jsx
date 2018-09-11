b754bdcebea73bc7227f276ab1cc49f3c0745162
navira5 <navira.ali@gmail.com>
2018-09-11 13:25:03 -0700

    Enable user signup on client

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,46 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TextField, Button, Icon } from '@material-ui/core'
+import axios from 'axios';
+import { TextField, Button, Icon } from '@material-ui/core';
 
 
-const Signup = (props) => {
-  return (
-    <div className='signup-container'>
-      <div className='signup-form-container' style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
-        <h1>Sign up</h1>
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
 
-        <form noValidate autoComplete='off' className='signup-form'>
-          <div style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
-            <TextField
-              fullWidth
-              required
-              id='username'
-              label='Enter a username'
-              placeholder='Username'
-              margin='normal'
-            >
-            </TextField>
-          </div>
-          <div style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto', marginBottom: '10px' }}>
-            <TextField
-              fullWidth
-              required
-              id='password'
-              label='Enter a password'
-              placeholder='password'
-              margin='normal'
-            >
-            </TextField>
-          </div>
-          <div style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
-            <Button variant="contained" size="small" className='signup-btn' color='primary' >
-              Sign up
+  setUser(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  setPassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleSignUp() {
+    axios.post('api/signup', {
+      username: this.state.username,
+      password: this.state.password
+    })
+    .then(response => {
+      console.log('New user added! response from server-->', response)
+    })
+    .catch(err => {
+      console.log('unable to add user to DB');
+    })
+  }
+
+  render() {
+    return (
+      <div className='signup-container'>
+        <div className='signup-form-container' style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
+          <h1>Sign up</h1>
+
+          <form noValidate autoComplete='off' className='signup-form'>
+            <div style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
+              <TextField
+                fullWidth
+                required
+                id='username'
+                label='Enter a username'
+                placeholder='Username'
+                margin='normal'
+                value={this.state.username}
+                onChange={this.setUser.bind(this)}
+              >
+              </TextField>
+            </div>
+            <div style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto', marginBottom: '10px' }}>
+              <TextField
+                fullWidth
+                required
+                id='password'
+                label='Enter a password'
+                placeholder='password'
+                margin='normal'
+                value={this.state.password}
+                onChange={this.setPassword.bind(this)}
+              >
+              </TextField>
+            </div>
+            <div style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto' }}>
+              <Button 
+                variant="contained" 
+                size="small" 
+                className='signup-btn' 
+                color='primary' 
+                onClick={this.handleSignUp.bind(this)}
+                >
+                Sign up
             </Button>
-          </div>
-        </form>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
+
 
 export default Signup;
