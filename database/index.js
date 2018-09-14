a16b471a0871963a406f4af82326455614408728
Michael Thomasson <mjthomasson@gmail.com>
2018-09-14 13:13:38 -0500

    moving facebook to correct routes2

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const saltRounds = 10;
 require('dotenv').config();
 // const mysql = require('mysql');
@@ -28,35 +28,35 @@ exports.selectAll = callback => {
   });
 };
 
-exports.saveUser = (username, password, callback) => {
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    const queryString = `INSERT INTO users (username, password) values ($1, $2)`;
-    pool.query(queryString, [username, hash], (err, results) => {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, results);
-      }
-    })
-  });
-}
+// exports.saveUser = (username, password, callback) => {
+//   bcrypt.hash(password, saltRounds, function (err, hash) {
+//     const queryString = `INSERT INTO users (username, password) values ($1, $2)`;
+//     pool.query(queryString, [username, hash], (err, results) => {
+//       if (err) {
+//         callback(err, null);
+//       } else {
+//         callback(null, results);
+//       }
+//     })
+//   });
+// }
 
-exports.validateUser = (loginName, loginPassword, callback) => {
-  const queryString = `select * from users where (username) like ($1)`;
-    pool.query(queryString, [loginName], (err, results) => {
-      const hash = results.rows[0].password;
-      bcrypt.compare(loginPassword, hash, (err, result) => {
-        if (err) {
-          callback(err, null);
-        } else {
-          if (!result) {
-            callback(null, 'Wrong Password')
-          } else {
-            callback(null, result);
-          }
-        }
-      });
-    });
-};
+// exports.validateUser = (loginName, loginPassword, callback) => {
+//   const queryString = `select * from users where (username) like ($1)`;
+//     pool.query(queryString, [loginName], (err, results) => {
+//       const hash = results.rows[0].password;
+//       bcrypt.compare(loginPassword, hash, (err, result) => {
+//         if (err) {
+//           callback(err, null);
+//         } else {
+//           if (!result) {
+//             callback(null, 'Wrong Password')
+//           } else {
+//             callback(null, result);
+//           }
+//         }
+//       });
+//     });
+// };
     
   
