d0086cb86b429249f92740409993b390b137dab5
Michael Thomasson <mjthomasson@gmail.com>
2018-09-14 13:56:47 -0500

    moving facebook to correct routes10

--- server/routes/createPost.js
+++ server/routes/createPost.js
@@ -20,24 +20,24 @@ router.post('/publish', (req, res) => {
   });
 });
 
-// router.get('/publish/twitter', function(req, res) {
-//   request.post({url:`https://api.twitter.com/1.1/statuses/update.json?status=Test%20tweet%20using%20the%20POST%20statuses%2Fupdate%20endpoint2`, oauth: twitter.oauth},
-//     function (e,r, body) {
-//       console.log('made it here');
-//       console.log(twitter.oauth);
+router.get('/publish/twitter', function(req, res) {
+  request.post({url:`https://api.twitter.com/1.1/statuses/update.json?status=Test%20tweet%20using%20the%20POST%20statuses%2Fupdate%20endpoint2`, oauth: twitter.oauth},
+    function (e,r, body) {
+      console.log('made it here');
+      console.log(twitter.oauth);
 
-//       console.log('\n\n\n\n\n\n\n\n\n\n');
-//       console.log('ERROR: ', e);
-//       console.log('\n\n\n\n\n\n\n\n\n\n');
-//       // console.log('R: ', r);
-//       // console.log('\n\n\n\n\n\n\n\n\n\n');
-//       console.log('Body: ', body);
-//       // console.log(user.results[0]);
-//       console.log('\n\n\n\n\n\n\n\n\n\n');
-//       // console.log(req);
-//       console.log('\n\n\n\n\n\n\n\n\n\n');
-//       res.send('all done');
-//     })
-// });
+      console.log('\n\n\n\n\n\n\n\n\n\n');
+      console.log('ERROR: ', e);
+      console.log('\n\n\n\n\n\n\n\n\n\n');
+      // console.log('R: ', r);
+      // console.log('\n\n\n\n\n\n\n\n\n\n');
+      console.log('Body: ', body);
+      // console.log(user.results[0]);
+      console.log('\n\n\n\n\n\n\n\n\n\n');
+      // console.log(req);
+      console.log('\n\n\n\n\n\n\n\n\n\n');
+      res.send('all done');
+    })
+});
 
 module.exports = router;
--- server/routes/debug.js
+++ server/routes/debug.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const twitter = require('../../utility/passport/twitter');
+const facebook = require('../../utility/passport/facebook');
+
 
 router.get('/log/credentials', function(req, res) {
   console.log(twitter.userData);
@@ -8,11 +10,11 @@ router.get('/log/credentials', function(req, res) {
   console.log('\n\n\n\n\n\n\n\n\n\n');
   console.log('token_secret: ', twitter.oauth.token_secret);
   console.log('\n\n\n\n\n\n\n\n\n\n');
-  console.log(facebookProfile);
+  console.log(facebook.userData);
   console.log('\n\n\n\n\n\n\n\n\n\n');
-  console.log('accessToken: ', fb_accessToken);
+  console.log('accessToken: ', facebook.oauth.accessToken);
   console.log('\n\n\n\n\n\n\n\n\n\n');
-  console.log('refreshToken: ', fb_refreshToken);
+  console.log('refreshToken: ', facebook.oauth.refreshToken);
   console.log('\n\n\n\n\n\n\n\n\n\n');
   res.send('credentials logged');
 });
