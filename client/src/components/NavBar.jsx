43b6f74de8b9e7181d9ced44bf76969334b3e9ce
Michael Thomasson <mjthomasson@gmail.com>
2018-09-14 13:13:06 -0500

    Add return to home functionality and navBar to CreatePost (#44)
    * Add routing and create post functionality
    
    * Receive data in server endpoints
    
    * Bundle update for new client code
    
    * Update gitignore
    
    * Update gitignore
    
    * Add return to home functionality and navBar to CreatePost
    
    * Delete bundle.js

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -21,7 +21,12 @@ class NavBar extends React.Component {
   }
 
   toggleDrawer() {
-    this.setState({ drawerOpen: !this.state.drawerOpen });
+    if (this.state.drawerOpen) {
+      this.setState({ drawerOpen: !this.state.drawerOpen }, 
+        this.props.history.push('/feed'));
+    } else {
+      this.setState({ drawerOpen: !this.state.drawerOpen });
+    }
   }
 
   handleLogout() {
@@ -46,7 +51,7 @@ class NavBar extends React.Component {
                   <ListItemIcon>
                     <CreateIcon />
                   </ListItemIcon>
-                  <ListItemText primary="Create New Post" />
+                  <ListItemText primary="Create New Post" onClick={ () => this.props.history.push('/createpost') }/>
                 </ListItem>
                 <ListItem button divider>
                   <ListItemIcon>
