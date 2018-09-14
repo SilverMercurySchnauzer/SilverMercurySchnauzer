8042e7927eea82358ed8534f14e192ba77e566c3
Michael Thomasson <mjthomasson@gmail.com>
2018-09-14 16:54:40 -0500

    Generate access token for twitter and use it to make and get tweets. (#45)
    * authenticate twitter and get and send tweets
    
    * added short explanation for what .env/heroku config variables should look like
    
    * Cleaned up code for pr
    
    * Delete newindex.js
    
    * Cleaned up code for pr2
    
    * changed redirects

--- server/routes/createPost.js
+++ server/routes/createPost.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const twitter = require('../../utility/passport/twitter');
+const request = require('request');
 
 router.get('/', (req, res) => {
   res.status(200).json({
@@ -18,6 +20,17 @@ router.post('/publish', (req, res) => {
   res.status(200).json({
     message: 'connected /createPost/publish POST'
   });
+});
+
+//Needs to be changed to a post (from client) and accept input from client
+router.get('/publish/twitter', function(req, res) {
+  request.post({url:`https://api.twitter.com/1.1/statuses/update.json?status=Test%20tweet%20using%20the%20POST%20statuses%2Fupdate%20endpoint2`, oauth: twitter.oauth},
+    function (e,r, body) {
+      console.log('Used twitter oauth headers: ', twitter.oauth);
+      console.log('ERROR: ', e);
+      console.log('Body: ', body);
+      res.send();
+    })
 });
 
 module.exports = router;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,11 +2,18 @@ const router = require('express').Router();
 const authenticate = require('./authenticate.js');
 const createPost = require('./createPost.js');
 const db = require('../../database');
-const twitter = require('../../utility/twitter');
-const facebook = require('../../utility/facebook');
+const twitter = require('../../utility/passport/twitter');
+const facebook = require('../../utility/passport/facebook');
+const passport = require('passport');
+require('dotenv').config();
+const session = require('express-session');
+const request = require('request');
+const debug = require('./debug.js');
 
+passport.use(twitter.strat);
 router.use('/createpost', createPost);
 router.use('/', authenticate);
+router.use('/debug', debug);
 
 router.get('/', (req, res) => {
   res.status(200).json({message: 'connected / GET'});
@@ -49,6 +56,13 @@ router.post('/items', (req, res) => {
 
 router.get('/home', (req, res) => {
     res.status(200).json({message: 'connected /api/home GET'});
+});
+
+router.get('/home/updateTwitterFeed', function (req, res) {
+  request.get({url:`https://api.twitter.com/1.1/statuses/user_timeline.json`, oauth: twitter.oauth},
+    function (error, response, body) {
+      res.send(body);
+    })
 });
 
 router.get('/drafts', (req, res) => {
