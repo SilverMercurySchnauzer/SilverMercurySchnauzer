fde22a62431bbf72ad5dc3c0f613b89393f68234
Michael Thomasson <mjthomasson@gmail.com>
2018-09-17 01:08:55 -0500

    Retrieve oauth tokens from db and retrieve tweets with them (#61)
    * was missing db function import at least
    
    * was missing db function import at least2
    
    * changed order of routes
    
    * create db retrieve function and test with live data
    
    * create db retrieve function and test with live data2
    
    * create db retrieve function and test with live data3
    
    * create db retrieve function and test with live data4
    
    * create db retrieve function and test with live data 5
    
    * retrieve oauth tokens from db and retrieve tweets with them

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const authenticate = require('./authenticate.js');
 const createPost = require('./createPost.js');
-const db = require('../../database');
+const db = require('../../database/index.js');
 const twitter = require('../../utility/passport/twitter');
 //const facebook = require('../../utility/passport/facebook');
 const passport = require('passport');
@@ -9,6 +9,7 @@ require('dotenv').config();
 const session = require('express-session');
 const request = require('request');
 const debug = require('./debug.js');
+const { retrieveTokens } = require('../../database/index');
 
 passport.use(twitter.strat);
 router.use('/createpost', createPost);
@@ -58,13 +59,24 @@ router.get('/home', (req, res) => {
     res.status(200).json({message: 'connected /api/home GET'});
 });
 
-router.get('/home/updateTwitterFeed', (req, res) => {
-  request.get({url:`https://api.twitter.com/1.1/statuses/user_timeline.json`, oauth: twitter.oauth}, (error, response, body) => {
-    // pull out required info from each tweet object and send back
-    let tweets = null;
-    res.send(tweets).status(200);
+router.get('/home/updateTwitterFeed/:userId', (req, res) => {
+  let userId = req.params.userId;
+  let oauth = twitter.oauth;
+  retrieveTokens(userId, (err, results) => {
+    if (err) {
+      console.log('Database/Server Error on retrieveTokens: ', err);
+    } else {
+      oauth.token = results.rows[0].twitter_token; 
+      oauth.token_secret = results.rows[0].twitter_token_secret;
+      request.get({url:`https://api.twitter.com/1.1/statuses/user_timeline.json`, oauth: oauth}, (error, response, body) => {
+        // pull out required info from each tweet object and send back
+        let tweets = null;
+        res.send(body).status(200);
+      })      
+    }
   })
 });
+
 
 router.get('/drafts', (req, res) => {
   res.status(200).json({message: 'connected /api/drafts GET'});
