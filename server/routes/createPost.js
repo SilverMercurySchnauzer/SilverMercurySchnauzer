a08b602cb4f2125c2dcb4a3f296482ab60cf7b3e
Michael Thomasson <mjthomasson@gmail.com>
2018-09-17 09:46:10 -0500

    Link publish button to publish tweet endpoint.  (#62)
    * send tweet to twitter
    
    * set loading to false
    
    * maybe try actually importing the function mike
    
    * maybe try proofreading what you ctrl+v mike
    
    * add logic check to retrieve tokens

--- server/routes/createPost.js
+++ server/routes/createPost.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { savePost } = require('../../database/index');
+const { savePost, retrieveTokens } = require('../../database/index');
 const twitter = require('../../utility/passport/twitter');
 const request = require('request');
 
@@ -25,11 +25,36 @@ router.post('/save', (req, res) => {
 
 
 router.post('/publish', (req, res) => {
-  console.log('body data to server /publish endpoint -->', req.body);
-  res.status(200).json({
-    message: 'connected /createPost/publish POST'
-  });
+  let userId = req.body.userId;
+  let oauth = twitter.oauth;
+  let qs = {
+    status: `${req.body.caption}  ${req.body.post}  ${req.body.url}`
+  }
+  retrieveTokens(userId, (err, results) => {
+    if (err) {
+      console.log('Database/Server Error on retrieveTokens: ', err);
+    } else {
+      oauth.token = results && results.rows ? results.rows[0].twitter_token : null; 
+      oauth.token_secret = results && results.rows ? results.rows[0].twitter_token_secret : null;
+      request.post({
+        url:`https://api.twitter.com/1.1/statuses/update.json`, 
+        oauth: oauth, 
+        qs: qs
+      }, (error, response, body) => {
+        console.log('Used twitter oauth headers: ', oauth);
+        console.log('Used qs: ', qs);
+        console.log('ERROR: ', error);
+        console.log('Body: ', body);
+        res.send().status(200);
+      })      
+    }
+  })
+  // console.log('body data to server /publish endpoint -->', req.body);
+  // res.status(200).json({
+  //   message: 'connected /createPost/publish POST'
+  // });
 });
+
 
 
 //Needs to be changed to a post (from client) and accept input from client
