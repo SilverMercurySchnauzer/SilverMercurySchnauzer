f6c22b3e1e178675dcc531f2ec7ebc579c03d9a5
Navira <30273156+navira5@users.noreply.github.com>
2018-09-17 14:18:22 -0500

    Add delete function in db and delete to scripts (#65)
    * verify date format in DB for save post
    
    * fixed cron

--- scripts/index.js
+++ scripts/index.js
@@ -11,29 +11,40 @@ exports.publishTweets = () => db.selectAll((err, results, fields) => {
     const fifteenMinsAgo = moment().subtract(15, 'minutes');
     results = results.rows.filter(t => moment(t.post_date, 'YYYY-MM-DDTHH:mm:ss').isAfter(fifteenMinsAgo));
     console.log('posts are ready to be published-->', results)
-    const publishThisPost = results[0];
-    let userId = publishThisPost.user_id;
-    let oauth = twitter.oauth;
-    let qs = {
-      status: `${publishThisPost.caption}  ${publishThisPost.text}  ${publishThisPost.media_url}`
-    }
-    db.retrieveTokens(userId, (err, results) => {
-      if (err) {
-        console.log('Database/Server Error on retrieveTokens: ', err);
-      } else {
-        oauth.token = results && results.rows ? results.rows[0].twitter_token : null;
-        oauth.token_secret = results && results.rows ? results.rows[0].twitter_token_secret : null;
-        request.post({
-          url: `https://api.twitter.com/1.1/statuses/update.json`,
-          oauth: oauth,
-          qs: qs
-        }, (error, response, body) => {
-          console.log('Used twitter oauth headers: ', oauth);
-          console.log('Used qs: ', qs);
-          console.log('ERROR: ', error);
-          console.log('Body: ', body);
-        })
+    //const publishThisPost = results[0];
+
+    results.forEach(publishThisPost => {
+      let userId = publishThisPost.user_id;
+      let oauth = twitter.oauth;
+      let qs = {
+        status: `${publishThisPost.caption}  ${publishThisPost.text}  ${publishThisPost.media_url}`
       }
+      db.retrieveTokens(userId, (err, results) => {
+        if (err) {
+          console.log('Database/Server Error on retrieveTokens: ', err);
+        } else {
+          oauth.token = results && results.rows ? results.rows[0].twitter_token : null;
+          oauth.token_secret = results && results.rows ? results.rows[0].twitter_token_secret : null;
+          request.post({
+            url: `https://api.twitter.com/1.1/statuses/update.json`,
+            oauth: oauth,
+            qs: qs
+          }, (error, response, body) => {
+            console.log('Used twitter oauth headers: ', oauth);
+            console.log('Used qs: ', qs);
+            console.log('ERROR: ', error);
+            console.log('Body: ', body);
+            db.deletePost(publishThisPost.id, (err, result) => {
+              if (err) {
+                console.log('err', err)
+              } else {
+                console.log('deleted post from DB', result);
+              }
+            })
+          })
+        }
+      })
     })
+    
   }
 });
