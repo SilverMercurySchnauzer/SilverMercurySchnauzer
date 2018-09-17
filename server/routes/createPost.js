4113bed5f9d003cc2540465b62bcc2004f79188c
Michael Thomasson <mjthomasson@gmail.com>
2018-09-17 01:45:49 -0500

    send tweet to twitter

--- server/routes/createPost.js
+++ server/routes/createPost.js
@@ -25,11 +25,35 @@ router.post('/save', (req, res) => {
 
 
 router.post('/publish', (req, res) => {
-  console.log('body data to server /publish endpoint -->', req.body);
-  res.status(200).json({
-    message: 'connected /createPost/publish POST'
-  });
+  let userId = req.body.userId;
+  let oauth = twitter.oauth;
+  let qs = {
+    status: `${req.body.caption}  ${req.body.post}  ${req.body.url}`
+  }
+  retrieveTokens(userId, (err, results) => {
+    if (err) {
+      console.log('Database/Server Error on retrieveTokens: ', err);
+    } else {
+      oauth.token = results.rows[0].twitter_token; 
+      oauth.token_secret = results.rows[0].twitter_token_secret;
+      request.post({
+        url:`https://api.twitter.com/1.1/statuses/update.json`, 
+        oauth: oauth, 
+        qs: qs
+      }, (error, response, body) => {
+        console.log('Used twitter oauth headers: ', twitter.oauth);
+        console.log('ERROR: ', e);
+        console.log('Body: ', body);
+        res.send().status(200);
+      })      
+    }
+  })
+  // console.log('body data to server /publish endpoint -->', req.body);
+  // res.status(200).json({
+  //   message: 'connected /createPost/publish POST'
+  // });
 });
+
 
 
 //Needs to be changed to a post (from client) and accept input from client
