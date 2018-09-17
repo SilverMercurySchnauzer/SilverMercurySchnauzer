c93e4fc6988497f6c656a06a0df4c73f18e74474
Michael Thomasson <mjthomasson@gmail.com>
2018-09-16 22:10:46 -0500

    was missing db function import at least

--- server/routes/oauth.js
+++ server/routes/oauth.js
@@ -3,6 +3,15 @@ const passport = require('passport');
 const twitter = require('../../utility/passport/twitter');
 const session = require('express-session');
 require('dotenv').config();
+const { updateToken } = require('../../database/index');
+const cors = require('cors');
+
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+router.use(cors({credentials : true, origin : ['https://pure-river-11017.herokuapp.com']}));
 
 router.get('/', (req, res) => {
   res.status(200).json({
@@ -11,12 +20,12 @@ router.get('/', (req, res) => {
 });
 
 router.get('/twitter/:userID', function (req, res, next) {
-  // console.log('req.session.state: ', req.session.state);
-  // console.log('\n\ntwitter login req.params.userID: ', req.params.userID);
+  console.log('req.session.state: ', req.session.state);
+  console.log('\n\ntwitter login req.params.userID: ', req.params.userID);
   if(req.params.userID !== 'authenticatedCallback'){
     req.session.state = req.params.userID;
   }
-  // console.log('\n\ntwitter login session.state #1: ', req.session.state);
+  console.log('\n\ntwitter login session.state #1: ', req.session.state);
   passport.authenticate('twitter', {session: false})(req, res, next)
 });
 
@@ -24,9 +33,9 @@ router.get('/twitter/:userID', function (req, res, next) {
 router.get('/twitter/authenticatedCallback', 
   passport.authenticate('twitter', { session: false, failureRedirect: '/' }),
   function(req, res) {
-    // console.log('\n\ntwitter login session.state #2: ', req.session.state);
+    console.log('\n\ntwitter login session.state #2: ', req.session.state);
     let userID = req.session.state;
-    // console.log('\n\nuserID: ', userID);
+    console.log('\n\nuserID: ', userID);
     updateToken( userID, 
       { 'provider': 'twitter', 
         'token': twitter.oauth.token, 
